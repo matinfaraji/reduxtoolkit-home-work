d877a1615673801bdb1e0cfde200606dc035c09e
matin faraji <matinfy1380@gmail.com>
2024-07-13 15:27:50 +0330

    redux headers

--- vite-project/src/components/Header/header.jsx
+++ vite-project/src/components/Header/header.jsx
@@ -1,25 +1,33 @@
 import bgDesktap from "../../assets/images/bg-header-desktop.svg";
 import { useSelector, useDispatch } from "react-redux";
-import {  clearTag } from "../../redux/reducers";
+import { clearTag, removeTag } from "../../redux/reducers";
 export default function Header() {
-    const selectedTags = useSelector((state) => state.filter.tags);
-    const dispatch = useDispatch();
-    const handelClickc=()=>{
-        dispatch(clearTag());
-      }
+  const selectedTags = useSelector((state) => state.filter.tags);
+  const dispatch = useDispatch();
+  const handelClickc = () => {
+    dispatch(clearTag());
+  };
+  const removeItem = (item) => {
+    dispatch(removeTag(item));
+  };
   return (
-    <div className="bg
-    ">
-      <img
-        src={bgDesktap}
-        alt=""
-        className="bg-desktep"/>
-        <div className="sort">
-            {selectedTags.map((item,index)=>(
-                <section className="sorted" key={index}>{item}</section>
-            ))}
-            <a onClick={handelClickc}>cleare</a>
-        </div>
+    <div
+      className="bg
+    "
+    >
+      <img src={bgDesktap} alt="" className="bg-desktep" />
+      <div className="sort">
+        {selectedTags.map((item, index) => (
+          <section
+            className="sorted"
+            key={index}
+            onClick={() => removeItem(item)}
+          >
+            {item}
+          </section>
+        ))}
+        <a onClick={handelClickc}>cleare</a>
+      </div>
     </div>
   );
 }
